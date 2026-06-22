32f5ac182bd5e2895a71bb005aa49ca538de9181
Diego Fernandez <diegofernandez.eu@gmail.com>
2026-06-22 20:39:54 +0200

    chore: include dev extras in install.sh

--- install.sh
+++ install.sh
@@ -6,7 +6,7 @@ python -m venv .venv
 # Activate virtual environment
 source .venv/bin/activate          # Windows: .venv\Scripts\activate
 
-# Install package in development mode
-pip install -e .
+# Install package in development mode with dev dependencies
+pip install -e ".[dev]"
 
 echo "Installation complete! Virtual environment activated."
